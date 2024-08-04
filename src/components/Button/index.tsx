a563ac394148768019ae509e9f56d37da5297ded
devprakashduve <dev876.1@gmail.com>
2024-08-04 13:56:47 +0530

    lint and prettier setupr

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+function Button() {
+  const handleClick = () => {
+    console.log("Why you click me?");
+  };
+  // this is test button
+  return (
+    <button type="button" onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+export default Button;
